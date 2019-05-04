207353d7715aca51a70a061c6c4cf96c83f7cc55
Brock Ross <m.brock.ross@gmail.com>
2019-05-04 15:54:46 -0600

    build basic scaffold for SCENES access
    Set up external file for storing scenes, and imported
    full module to App component for use in state.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
+import * as SCENES from '../scenes.js';
+
 class App extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      currScene: SCENES.SCENE_T1
+    }
   }
 
   render() {
     return (
-      <div>yoooooooo</div>
+      <div>
+        <p>{this.state.currScene.text}</p>
+        <p>{this.state.currScene.options.opt1.text}</p>
+      </div>
     )
   }
 }
--- client/src/scenes.js
+++ client/src/scenes.js
@@ -6,7 +6,7 @@
 
  */
 
-const SCENE_T1 = {
+export var SCENE_T1 = {
     text: "Hey, I'm Jerry the Giraffe! Want to go on an adventure?",
     options: {
       opt1: {
@@ -15,13 +15,13 @@ const SCENE_T1 = {
       },
       opt2: {
         text: "No thanks.",
-        next: EXIT_SCENE
+        next: '' //EXIT_SCENE
       }
     },
     illustration: 'dummy-image.jpg'
   }
 
-const SCENE_T2 = {
+export var SCENE_T2 = {
   text: "I'm at a fork in the road. One way leads to the jungle where I live, and the other way leads to the big city. Which way should we go?",
   options: {
     opt1: {
@@ -37,31 +37,31 @@ const SCENE_T2 = {
 }
 
 //======= JUNGLE PATH =======//
-const SCENE_J01 = {
+export var SCENE_J01 = {
   text: "We made it to the jungle! I see the path to my house weaving through the trees. Should we go home, or climb a tree?",
   options: {
     opt1: {
       text: "Let's go home.",
-      next: SCENE_END01
+      next: '' //SCENE_END01
     },
     opt2: {
       text: "Let's climb a tree!",
-      next: SCENE_J02
+      next: ''//SCENE_J02
     }
   }
 }
 
 //======= CITY PATH =======//
-const SCENE_C01 = {
+export var SCENE_C01 = {
   text: "Wow, there's so much to see here in the city! Ooh, what's that on the ground? Something shiny...",
   options: {
     opt1: {
       text: "Pick it up!",
-      next: SCENE_C02
+      next: ''//SCENE_C02
     },
     opt2: {
       text: "Leave it alone.",
-      next: SCENE_C02
+      next: ''//SCENE_C02
     }
   }
 }
